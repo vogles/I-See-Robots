dbaf37b88e0c6542ef22a5f811b10d40d4f61496
Bisma J <janjubis@gmail.com>
2014-01-25 18:11:30 -0800

    added title page

---
+++ Unity/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour {
+
+	public bool start = false;
+	public bool opti = false;
+	public bool quit = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnClick () {
+		//counter++;
+		//print ("Clicked " + counter.ToString () + " times.");
+
+		if (start) {
+			Application.LoadLevel("02_Training");
+		}
+
+		if (opti) {
+			Application.LoadLevel("00_Options");
+		}
+
+		if (quit) {
+			Application.Quit();
+		}
+
+	}
+}
