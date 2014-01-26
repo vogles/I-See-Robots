da443039b6641f22b16215fa9ab84a8deac676be
Bisma J <janjubis@gmail.com>
2014-01-26 14:26:13 -0800

    health bar

---
+++ Unity/Assets/Scripts/HealthBar.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+ 
+// JavaScript
+ 
+var backgroundTexture : Texture;
+var foregroundTexture : Texture;
+ 
+var healthWidth: int = 199;
+var healthHeight: int = 30;
+ 
+var healthMarginLeft: int = 41;
+var healthMarginTop: int = 38;
+ 
+var frameWidth : int = 266;
+var frameHeight: int = 65;
+ 
+var frameMarginLeft : int = 10;
+var frameMarginTop: int = 10;
+ 
+function OnGUI () {
+ 
+    GUI.DrawTexture( Rect(frameMarginLeft,frameMarginTop, frameMarginLeft + 
+    frameWidth, frameMarginTop + frameHeight), backgroundTexture, 
+    ScaleMode.ScaleToFit, true, 0 );
+ 
+    GUI.DrawTexture( Rect(healthMarginLeft,healthMarginTop,
+    healthWidth + healthMarginLeft, healthHeight), foregroundTexture, 
+    ScaleMode.ScaleAndCrop, true, 0 );
+ }
+ 
+ function Update() {
+ 		loseHealth();
+ 
+ }
+ 
+ 
+ function loseHealth() {
+ 
+ 	if (healthWidth > 0) {
+ 	healthWidth = healthWidth - .1;
+ 	
+ 	}
+  
+ }
+ 
---
+++ Unity/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour 
+{
+	public GUISkin myskin;
+	
+	private Rect windowRect;
+	private bool paused = false , waited = true;
+	
+	private void Start()
+	{
+		windowRect = new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 200);
+	}
+	
+	private void waiting()
+	{
+		waited = true;
+	}
+	
+	private void Update()
+	{
+		if (waited)
+		{
+			if (Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.P))
+			{
+				if (paused)
+					paused = false;
+				else
+					paused = true;
+				
+				waited = false;
+				Invoke("waiting",0.3f);
+			}
+		}
+		if (paused)
+			Time.timeScale = 0;
+		else
+			Time.timeScale = 1;
+	}
+	
+	private void OnGUI()
+	{
+		if (paused)
+			windowRect = GUI.Window(0, windowRect, windowFunc, "Pause Menu");
+	}
+	
+	private void windowFunc(int id)
+	{
+		if (GUILayout.Button("Resume Game"))
+		{
+			paused = false;
+		}
+		if (GUILayout.Button("Retry"))
+		{
+			Application.LoadLevel("Level 1");
+		}
+		if (GUILayout.Button("Quit"))
+		{
+			Application.LoadLevel("StartMenu");
+		}
+	}
+}
